d7ca891a9cd6834aebd7620ca79b33822a223fe7
bueongi <67132017+bueongi@users.noreply.github.com>
2020-11-10 10:32:13 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -1,10 +1,8 @@
-// 빈 링크
 $('.sub_nav a[href="#"]').on('click',function(){
-  alert('서비스 준비중입니다.')
+  alert('서비스 준비중입니다.');
 });
-// 빈 링크 리턴 방지
+
 $('.section.sns .sns_img').on('click',function(){
   return false;
 });
-
 
