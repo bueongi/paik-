4441fe3b100bdc962d31bd0ce6b29af3e5b8b2c3
bueongi <67132017+bueongi@users.noreply.github.com>
2020-11-14 14:49:46 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -1,3 +1,26 @@
+// 서브내비 호출
+function closeSub(){
+  $('.nav>li').removeClass('active');
+  $('.nav>li').find('.sub_nav').slideUp(100);
+}
+$('.nav>li').hover(function(){
+  if($(this).hasClass('active')){
+    $(this).removeClass('active');
+    $(this).closest('.nav').removeClass('change');
+    $(this).find('.sub_nav').slideUp(100);
+  }else{
+    closeSub();
+    $(this).addClass('active');
+    $(this).closest('.nav').addClass('change');
+    $(this).find('.sub_nav').slideDown(300);
+  };
+  $('.sub_nav').mouseleave(function(){
+    $(this).closest('.nav').removeClass('change');
+    $(this).slideUp(100);
+  });
+});
+
+
 $('.sub_nav a[href="#"]').on('click',function(){
   alert('서비스 준비중입니다.');
 });
